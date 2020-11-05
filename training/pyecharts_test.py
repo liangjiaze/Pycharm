a3e406ad0d80b9b9103a7684b1b752a0b97dc260
梁家泽 <renmuda@163.com>
2020-11-05 11:36:15 +0800

    add 1、测试pyecharts组件

--- training/pyecharts_test.py
+++ training/pyecharts_test.py
@@ -1,4 +1,33 @@
-from pyecharts import options as opts
+import pyecharts
+
+print(pyecharts.__version__)
+# pyecharts官方文档：https://pyecharts.org/#/zh-cn/quickstart
+
+# --------------------------------------------------添加第一个柱状图----------------------------------------------------
 from pyecharts.charts import Bar
-from pyecharts.commons.utils import JsCode
+from pyecharts import options as opts
+# 设置图形主题，内置主题类型可查看 pyecharts.globals.ThemeType
 from pyecharts.globals import ThemeType
+
+filepath = r'D:\jade\Desktop\myEcharts0.html'
+bar = Bar(init_opts=opts.InitOpts(theme=ThemeType.LIGHT))
+bar.add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
+bar.add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
+bar.add_yaxis("商家B", [15, 6, 45, 20, 35, 66])
+bar.set_global_opts(title_opts=opts.TitleOpts(title="主标题", subtitle="副标题"))
+bar.render(filepath)
+
+# ----------------------------------pyecharts所有方法均支持链式调用，等价于下面代码：-----------------------------------
+# from pyecharts.charts import Bar
+# from pyecharts import options as opts
+#
+# filepath = r'D:\jade\Desktop\myEcharts1.html'
+# bar = (
+#     Bar()
+#         .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
+#         .add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
+#         .set_global_opts(title_opts=opts.TitleOpts(title="主标题", subtitle="副标题"))
+#     # 或者直接使用字典参数
+#     # .set_global_opts(title_opts={"text": "主标题", "subtext": "副标题"})
+# )
+# bar.render(filepath)
